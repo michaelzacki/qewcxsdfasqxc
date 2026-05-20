5b40dbbc2c0d5afb74e5e1bd1802623a3aa4e8b0
michaelzacki <trfreshhesap@gmail.com>
2026-05-20 22:02:15 +0300

    Update sync.js

--- api/sync.js
+++ api/sync.js
@@ -145,7 +145,7 @@ export default async function handler(req, res) {
       p.talismans = data.talismans ?? p.talismans;
       p.stats = data.stats ?? p.stats;
 
-      await redis.hset('globals_hash', player_id, JSON.stringify(p));
+      await redis.hset('globals_hash', { [player_id]: p });
       return res.status(200).json({ success: true });
     } catch (error) {
       return res.status(500).json({ error: 'Write error' });
